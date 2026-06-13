c7577d468033085c5c79f14868d2777c029e05e4
tfyf103 <1148777130@qq.com>
2026-06-14 01:31:50 +0800

    实现个人主页后端，实现个人主页用户信息区

--- backend/web/urls.py
+++ backend/web/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, re_path
 
 from web.create.character.create import CreateCharacterView
+from web.create.character.get_list import GetListCharacterView
 from web.create.character.get_single import GetSingleCharacterView
 from web.create.character.remove import RemoveCharacterView
 from web.create.character.update import UpdateCharacterView
@@ -23,6 +24,7 @@ urlpatterns = [
     path('api/create/character/update/', UpdateCharacterView.as_view()),
     path('api/create/character/remove/', RemoveCharacterView.as_view()),
     path('api/create/character/get_single/', GetSingleCharacterView.as_view()),
+    path('api/create/character/get_list/', GetListCharacterView.as_view()),
     path('',index),
     re_path(r'^(?!media/|static/|assets/).*$', index),
 ]
