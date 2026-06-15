f96da725bbfd77aa9766f70786a6061f35f8dcb9
tfyf103 <1148777130@qq.com>
2026-06-15 22:37:46 +0800

    实现首页前后端

--- backend/web/urls.py
+++ backend/web/urls.py
@@ -5,6 +5,7 @@ from web.create.character.get_list import GetListCharacterView
 from web.create.character.get_single import GetSingleCharacterView
 from web.create.character.remove import RemoveCharacterView
 from web.create.character.update import UpdateCharacterView
+from web.views.homepage.index import HomepageIndexView
 from web.views.index import index
 from web.views.user.account.get_user_info import GetUserInfo
 from web.views.user.account.login import LoginView
@@ -25,6 +26,7 @@ urlpatterns = [
     path('api/create/character/remove/', RemoveCharacterView.as_view()),
     path('api/create/character/get_single/', GetSingleCharacterView.as_view()),
     path('api/create/character/get_list/', GetListCharacterView.as_view()),
+    path('api/homepage/index/', HomepageIndexView.as_view()),
     path('',index),
     re_path(r'^(?!media/|static/|assets/).*$', index),
 ]
