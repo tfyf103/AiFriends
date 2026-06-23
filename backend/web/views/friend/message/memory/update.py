57f4c78c35313360065169c8ff008c77bba914a4
tfyf103 <1148777130@qq.com>
2026-06-23 21:11:10 +0800

    添加知识库

--- backend/web/views/friend/message/memory/update.py
+++ backend/web/views/friend/message/memory/update.py
@@ -1,5 +1,3 @@
-from pprint import pprint
-
 from django.utils.timezone import now
 from langchain_core.messages import SystemMessage, HumanMessage
 
@@ -33,10 +31,8 @@ def update_memory(friend):
         ]
     }
 
-    pprint(inputs)
     res = app.invoke(inputs)
     friend.memory = res['messages'][-1].content
-    pprint(friend.memory)
 
     friend.update_time = now()
     friend.save()
